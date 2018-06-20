0502c4691dff02f78611d22c925f3b0081f262ec
Administrator <admin@example.com>
2018-06-20 14:41:54 +0800

    editcontent

--- application/index/Module.php
+++ application/index/Module.php
@@ -4,6 +4,15 @@ namespace app\index;
 use think\Controller;
 
 class Module extends Controller{
+    public function __construct()
+    {
+
+    }
+
+    public function checkRole(){
+
+    }
+
     public function test(){
         echo "hello word!!!";
     }
