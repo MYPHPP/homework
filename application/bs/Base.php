a8d075ec34b085325cb6ed0f4bb6275e050f0939
Administrator <admin@example.com>
2018-07-07 17:42:00 +0800

    调整

--- application/bs/Base.php
+++ application/bs/Base.php
@@ -5,6 +5,7 @@ use app\common\model\Menu;
 use think\cache\driver\Redis;
 use think\Controller;
 use think\Request;
+use think\Validate;
 
 class Base extends Controller {
     public function __construct(Request $request)
@@ -18,5 +19,78 @@ class Base extends Controller {
         $method = strtolower($request->controller());
         $action = strtolower($request->action());
         $menuid = Menu::where(['module'=>$module,'method'=>$method,'action'=>$action])->value('id');
+    }
+
+    public function getMenu($modelname,$choose="default"){
+        $useModel = "\\app\\common\\model\\".$modelname;
+        $model = new $useModel;
+        $menus = $model->getCF($choose);
+        foreach($menus[$choose] as $key=>$menu){
+            if(empty($menu['name'])){
+                $menus[$choose][$key]['name'] = $key;
+            }
+        }
+        $result = array2obj($menus);
+        return $result;
+    }
+
+    public function checkData(Request $request,$useModel,$choose="default"){
+        $rule = [];
+        $tipMsg = [];
+        $data = [];
+        $menus = $this->getMenu($useModel,$choose);
+        foreach ($menus->{$choose} as $menu){
+            if(isset($request->{$menu->name})){
+                if(!empty($menu->validate)){
+                    $rule[$menu->name] = $menu->validate->rule;
+                    $tips = explode('|',$menu->validate->rule);
+                    foreach ($tips as $tip){
+                        if(strrpos($tip,':')){
+                            $tip = substr($tip,0,strrpos($tip,':'));
+                        }
+                        $tipMsg[$menu->name.".".$tip] = $menu->validate->{$menu->name.".".$tip};
+                    }
+                }
+                $data[$menu->name] = filterChar($request->{$menu->name});
+            }
+        }
+        $result = ['status'=>200,'data'=>$data];
+        if(!empty($rule)){
+            //$validate = Validate::make($rule,$tipMsg)->batch();//批量验证
+            $validate = Validate::make($rule,$tipMsg);//单个验证
+            if(!$validate->check($data)){
+                $result = ['status' => 301,'data'=>$validate->getError()];
+            }
+        }
+        return $result;
+    }
+
+    public function checkUnique($modelname,$data,$choose="default"){
+        $check = [];
+        $useModel = "\\app\\common\\model\\".$modelname;
+        $model = new $useModel;
+        $menus = $this->getMenu($modelname,$choose);
+        foreach($menus->{$choose} as $menu){
+            if(isset($data[$menu->name])){
+                if(isset($menu->unique) && $menu->unique == true){
+                    $check[] = [$menu->name=>$data[$menu->name]];
+                }
+            }
+        }
+        $res = true;
+        if(!empty($check)){
+            $query = $model->where($check[0]);
+            if(count($check) > 1){
+                unset($check[0]);
+                foreach($check as $val){
+                    $query = $query->whereOr($val);
+                }
+            }
+            $result = $query->find();
+            if(!empty($result)){
+                $res = false;
+            }
+        }
+        return $res;
     }
 }
--- application/bs/controller/Index.php
+++ application/bs/controller/Index.php
@@ -1,10 +1,11 @@
 <?php
 namespace app\bs\controller;
 
+use app\bs\Base;
 use app\common\model\User;
 use think\Controller;
 use think\Request;
-use think\Validate;
+
 
 class Index extends Controller{
     protected $useModel = "User";
@@ -13,6 +14,7 @@ class Index extends Controller{
      * 登录
      * */
     public function index(Request $request){
+        $quoteModel = new Base($request);
         if(!empty(cookie('login_id'))){
             session('login_id',cookie('login_id'));
             if(!empty(cookie("currentUrl"))){
@@ -22,7 +24,7 @@ class Index extends Controller{
             }
         }
         if($request->isPost()){
-            $result = $this->checkData($request,$this->useModel,$this->choose);
+            $result = $quoteModel->checkData($request,$this->useModel,$this->choose);
             if($result['status'] != 200){
                 if(is_array($result['data'])){
                     $msg = implode(',',$result['data']);
@@ -46,7 +48,7 @@ class Index extends Controller{
                 $this->error('账号或密码错误');
             }
         }
-        $menu = $this->getMenu($this->useModel,$this->choose);
+        $menu = $quoteModel->getMenu($this->useModel,$this->choose);
         $this->assign('menus',$menu->{$this->choose});
         return $this->fetch('login');
     }
@@ -55,8 +57,9 @@ class Index extends Controller{
      * 注册
      * */
     public function register(Request $request){
+        $quoteModel = new Base($request);
         if($request->isPost()){
-            $result = $this->checkData($request,$this->useModel,$this->choose);
+            $result = $quoteModel->checkData($request,$this->useModel,$this->choose);
             if($result['status'] != 200){
                 if(is_array($result['data'])){
                     $msg = implode(',',$result['data']);
@@ -65,7 +68,7 @@ class Index extends Controller{
                 }
                 $this->error($msg);
             }
-            $checkResult = $this->checkUnique($this->useModel,$result['data'],$this->choose);
+            $checkResult = $quoteModel->checkUnique($this->useModel,$result['data'],$this->choose);
             if($checkResult){
                 $modelname = "\\app\\common\\model\\".$this->useModel;
                 $model = new $modelname;
@@ -75,81 +78,8 @@ class Index extends Controller{
                 $this->error("账号已存在");
             }
         }
-        $menu = $this->getMenu($this->useModel,$this->choose);
+        $menu = $quoteModel->getMenu($this->useModel,$this->choose);
         $this->assign('menus',$menu->{$this->choose});
         return $this->fetch('register');
-    }
-
-    public function getMenu($modelname,$choose="default"){
-        $useModel = "\\app\\common\\model\\".$modelname;
-        $model = new $useModel;
-        $menus = $model->getCF($choose);
-        foreach($menus[$choose] as $key=>$menu){
-            if(empty($menu['name'])){
-                $menus[$choose][$key]['name'] = $key;
-            }
-        }
-        $result = array2obj($menus);
-        return $result;
-    }
-
-    public function checkData(Request $request,$useModel,$choose="default"){
-        $rule = [];
-        $tipMsg = [];
-        $data = [];
-        $menus = $this->getMenu($useModel,$choose);
-        foreach ($menus->{$choose} as $menu){
-            if(isset($request->{$menu->name})){
-                if(!empty($menu->validate)){
-                    $rule[$menu->name] = $menu->validate->rule;
-                    $tips = explode('|',$menu->validate->rule);
-                    foreach ($tips as $tip){
-                        if(strrpos($tip,':')){
-                            $tip = substr($tip,0,strrpos($tip,':'));
-                        }
-                        $tipMsg[$menu->name.".".$tip] = $menu->validate->{$menu->name.".".$tip};
-                    }
-                }
-                $data[$menu->name] = filterChar($request->{$menu->name});
-            }
-        }
-        $result = ['status'=>200,'data'=>$data];
-        if(!empty($rule)){
-            //$validate = Validate::make($rule,$tipMsg)->batch();//批量验证
-            $validate = Validate::make($rule,$tipMsg);//单个验证
-            if(!$validate->check($data)){
-                $result = ['status' => 301,'data'=>$validate->getError()];
-            }
-        }
-        return $result;
-    }
-
-    public function checkUnique($modelname,$data,$choose="default"){
-        $check = [];
-        $useModel = "\\app\\common\\model\\".$modelname;
-        $model = new $useModel;
-        $menus = $this->getMenu($modelname,$choose);
-        foreach($menus->{$choose} as $menu){
-            if(isset($data[$menu->name])){
-                if(isset($menu->unique) && $menu->unique == true){
-                    $check[] = [$menu->name=>$data[$menu->name]];
-                }
-            }
-        }
-        $res = true;
-        if(!empty($check)){
-            $query = $model->where($check[0]);
-            if(count($check) > 1){
-                unset($check[0]);
-                foreach($check as $val){
-                    $query = $query->whereOr($val);
-                }
-            }
-            $result = $query->find();
-            if(!empty($result)){
-                $res = false;
-            }
-        }
-        return $res;
     }
 }
