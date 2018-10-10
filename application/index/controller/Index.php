dc576dad9969e0dc403544064721d30b897d95c0
Administrator <admin@example.com>
2018-10-10 16:35:09 +0800

    test

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -36,5 +36,36 @@ class Index extends Module
         echo $redis->get('test');
     }
 
-    public function test(){}
+    public function test(){
+        $mcg = ['北京市','天津市','上海市','重庆市'];
+        $ar = ['内蒙古自治区','广西壮族自治区','西藏自治区','宁夏回族自治区','新疆维吾尔自治区'];
+        $content = file_get_contents('./aa.csv');
+        $arrs = explode(PHP_EOL,$content);
+        $addData = [];
+        foreach($arrs as $k=>$arr){
+            $local = explode(',',$arr);
+            $reg1 = '/^('.implode('|',$mcg).')[\x{4e00}-\x{9fa5}]*$/u';
+            $reg2 = '/^('.implode('|',$ar).')[\x{4e00}-\x{9fa5}]*$/u';
+            if(preg_match($reg1,$local[1],$matches)){
+                $province = '';
+                $city = mb_substr($local[1],0,3);
+                $area = '';
+                if(strlen($local[1]) > 9){
+                    $area = mb_substr($local[1],3);
+                }
+            }else if(preg_match($reg2,$local[1],$matches)){
+                $province = $matches[1];
+                $remaining = str_replace($province,'',$local[1]);
+                echo $remaining.'<br/>';
+            }
+//            $province = '';
+//            $city = '';
+//            $area = '';
+//            $addData[$k]['province'] = $province;
+//            $addData[$k]['city'] = $city;
+//            $addData[$k]['area'] = $area;
+//            $addData[$k]['address'] = $local[1];
+        }
+        dd($addData);
+    }
 }
