6fb616efdabd8d3cf0bcebfeb68655f1c4976451
fengbo <ycpfb>
2019-01-08 15:26:17 +0800

    change

--- public/js/menu.js
+++
@@ -1,7 +0,0 @@
-$("#icon").change(function () {
-    var choose = $(this).val();
-    var con = $(this).prev().text();
-    var icon = '<i class="'+choose+'"></i>';
-    var text = con+icon;
-    $(this).prev().html(text);
-});
