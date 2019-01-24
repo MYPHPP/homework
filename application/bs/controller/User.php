b37ecb589e6f9988bd6d6b439c8a0942bab15a6d
fengbo <ycpfb>
2019-01-24 18:13:55 +0800

    profile

--- application/bs/controller/User.php
+++ application/bs/controller/User.php
@@ -7,6 +7,14 @@ use app\common\model\User as Users;
 class User extends Base{
     public function index()
     {
+        if($this->request->isPost()){
+            $data = $this->request->param();
+            if(Users::update($data,['id'=>$this->loginUserinfo->id])){
+                $this->success('修改成功！');
+            }else{
+                $this->error('修改失败，请检查数据！');
+            }
+        }
         return $this->fetch();
     }
 }
