a000e732c2a1ab02079933cf987b8da9b291dc5c
fengbo <ycpfb>
2019-01-25 15:30:17 +0800

    login check

--- application/bs/controller/Index.php
+++ application/bs/controller/Index.php
@@ -5,6 +5,7 @@ use app\common\model\User;
 use think\Controller;
 use think\Request;
 use app\common\model\Menu;
+use tools\GeetestLib;
 
 class Index extends Controller{
     /*
@@ -29,7 +30,11 @@ class Index extends Controller{
     public function index(Request $request){
         $model = new User;
         if($model->checkLogin()){
-            return redirect($this->loginJump());
+            if($this->loginJump()){
+                return redirect($this->loginJump());
+            }else{
+                return $this->fetch('./404');
+            }
         }
         if($request->isPost()){
             if(empty($request->name) || empty($request->passwd)){
@@ -41,6 +46,10 @@ class Index extends Controller{
                 if($user->passwd['change'] != $request->passwd){
                     $this->error('密码错误!');
                 }
+                //验证码验证
+                if(!$this->check_geetest($request)){
+                    $this->error('请先进行验证操作!');
+                }
                 session('login_id',$user->id);
                 session('login_pwd',$user->passwd['original']);
                 if(!empty($request->remember)){
@@ -48,9 +57,9 @@ class Index extends Controller{
                     cookie("ms_login_pwd",$user->passwd['original'],7*86400);
                 }
                 if(!empty(cookie("ms_currentUrl"))){
-                    return redirect(cookie("ms_currentUrl"));
+                    $this->success('登录成功',cookie("ms_currentUrl"));
                 }else{
-                    return redirect($this->loginJump());
+                    $this->success('登录成功',$this->loginJump());
                 }
             }else{
                 $this->error('用户不存在!');
@@ -71,12 +80,13 @@ class Index extends Controller{
     /*
      * 登录成功页面跳转
      * */
-    public function loginJump(){
-        $url = 'bs/index/abort';
+    public function loginJump(){;
         if(empty(cookie('ms_currentUrl'))){
             $menus = Menu::getUserMenu("route");
             if(!empty($menus) && $menus->count() > 0){
                 $url = $this->chooseUrl($menus);
+            }else{
+                $url = false;
             }
         }else{
             $url = cookie('ms_currentUrl');
@@ -104,5 +114,42 @@ class Index extends Controller{
             }
             cookie('pr_pagerow',$num);
         }
+    }
+
+    //使用前验证
+    public function get_geetest_status(Request $request)
+    {
+        $geetest = new GeetestLib(config('setting.geetest.id'), config('setting.geetest.key'));
+        $data    = array(
+            "user_id"     => "0", # 网站用户id
+            "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
+            "ip_address"  => $request->ip() # 请在此处传输用户请求验证时所携带的IP
+        );
+        $status = $geetest->pre_process($data, 1);
+        session('gtserver',$status);
+        session('gt_user_id',$data['user_id']);
+        return json($geetest->get_response_str());
+    }
+
+    protected function check_geetest(Request $request)
+    {
+        $geetest = new GeetestLib(config('setting.geetest.id'), config('setting.geetest.key'));
+        $data    = array(
+            "user_id"     => session('gt_user_id'), # 网站用户id
+            "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
+            "ip_address"  => $request->ip()
+        );
+
+        if (session('gtserver') == 1) {   //服务器正常
+            $result = $geetest->success_validate($request->param('geetest_challenge'), $request->param('geetest_validate'), $request->param('geetest_seccode'), $data);
+            if ($result) {
+                return true;
+            }
+        } else {  //服务器宕机,走failback模式
+            if ($geetest->fail_validate($request->param('geetest_challenge'), $request->param('geetest_validate'))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
